3ad59a124a3414c71fa08abba5c843aa1c26fcb1
Fernanda Marques <fermarquessantos@gmail.com>
2023-09-12 17:54:27 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,33 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Steve Carell toparia voltar ao The Office?",
+        "description": "Após o fim da última temporada em 2013, os fãs especulam o retorno da série com o World's Best Boss.",
+        "photoCover": "https://cdn.pastemagazine.com/www/articles/MikeScott.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Rumores sobre nova temporada de The Office",
+        "description": "O que todo fã gostaria de ouvir",
+        "photoCover": "https://veja.abril.com.br/wp-content/uploads/2016/11/s9to-elenco.jpg?quality=70&strip=all"
+    },
+    {
+        "id":"3",
+        "title":"Dez anos atrás foi ao ar o último episódio da série",
+        "description": "O ano era 2013",
+        "photoCover": "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/05/the-office-metaverso-1.jpeg"
+
+    },
+    {
+        "id":"4",
+        "title":"Melhores momentos na Dunder Mifflin Inc",
+        "description": "Eles compartilharam os melhores momentos da Paper Company",
+        "photoCover": "https://s2-techtudo.glbimg.com/y9dqURILdsr5wEVxrF_PoI9EsPM=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/X/s/DtXUAyRLCB6hx8Xy5TRg/the-office-2.jpg"
+    },
+    {
+        "id":"5",
+        "title":"Saiba mais sobre as personagens de The Office",
+        "description":"Busque um número e conheça os nomes, cargo e quem interpreta a personagem na série.",
+        "photoCover": "https://m.media-amazon.com/images/I/51lu881qZmL._AC_UF894,1000_QL80_.jpg"
+    }
+]
